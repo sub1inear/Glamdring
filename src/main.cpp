d4b10093e6b4442680e005307f4185dc476691e4
unknown <odoul28@gmail.com>
2025-08-15 19:52:36 -0700

    Ensure consistant working directory in VS and on command line

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv) {
     chess_t chess;
 
-    bool result = chess.opening_book.set_book("../data/Titans.bin");
+    bool result = chess.opening_book.set_book("Titans.bin");
     if (!result) {
         chess.print_uci("fopen() in opening_book_t::set_book() failed: %s\n", strerror(errno));
         return 1;
